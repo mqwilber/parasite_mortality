c5fabc626c8eb62e4ad460d3b371b1a289ea69f4
Mark Wilber <mqwilber@gmail.com>
2014-10-24 15:22:48 -0700

    Code it giving reasonable predictions

--- code/parasite_morality_code.R
+++ code/parasite_morality_code.R
@@ -1,6 +1,6 @@
 library(MASS)
 
-file = "/Users/mqwilber/Desktop/worm_data.csv"
+file = "../data/worm_data.csv"
 data=read.csv(file)
 
 bp <- data$adBPtot
@@ -22,81 +22,84 @@ pred = probs * length(bp)  # Maybe set fractional raccoons to zero?
 # # Observed can't be greater than predicted...seems like a hack-
 # #is binomial the best assumed distribution- cant have obs >pred
 
-# parasite_nums = seq(0, max(bp), 1)
-# new_pred = round(pred)
-
-# new_obs = obs
-# ind = obs > new_pred
-# new_obs[ind] = new_pred[ind]
+parasite_nums = seq(0, max(bp), 1)
+new_pred = round(pred)
+new_obs = obs
+ind = obs > new_pred
+new_obs[ind] = new_pred[ind]
 
 # Drop where pred is zero- removes fraction individuals
 # start at i=2 because dont include 0 class as should have 100%surv
-# ind_nozeros = new_pred != 0
-# trun_obs = new_obs[ind_nozeros][2:sum(ind_nozeros)]
-# trun_pred = new_pred[ind_nozeros][2:sum(ind_nozeros)]
-# trun_x = parasite_nums[ind_nozeros][2:sum(ind_nozeros)]
+ind_nozeros = new_pred != 0
+trun_obs = new_obs[ind_nozeros][2:sum(ind_nozeros)]
+trun_pred = new_pred[ind_nozeros][2:sum(ind_nozeros)]
+trun_x = parasite_nums[ind_nozeros][2:sum(ind_nozeros)]
 
 
 # # Maximum likelihood function
 
-# logistregfun = function(params, x, N, y){
+logistregfun = function(params, x, N, y){
 
-# 	a = params[1]
-# 	b = params[2]
-# 	p.pred = exp(a + b *log( x)) / (1 + exp(a + b * log(x)))
-# 	-sum(dbinom(y, size=N, prob=p.pred, log=TRUE))
-# }
+	a = params[1]
+	b = params[2]
+	p.pred = exp(a + b *log(x)) / (1 + exp(a + b * log(x)))
+	-sum(dbinom(y, size=N, prob=p.pred, log=TRUE))
+}
 
-# opt1 = optim(fn = logistregfun, par = c(a=.5,b=.5),
-# 				x = trun_x, N=trun_pred, y=trun_obs,
-# 				method="BFGS")
+opt1 = optim(fn = logistregfun, par = c(a=.5,b=.5),
+				x = trun_x, N=trun_pred, y=trun_obs,
+				method="BFGS")
 
-# hx<-trun_obs / trun_pred
+hx<-trun_obs / trun_pred
 
 # # Plotting MLE and data
 
-# a=opt1$par[1]
-# b=opt1$par[2]
-# x1=seq(1, 75,1)
-# y<-exp(a + b * log(x1)) / (1 + exp(a + b * log(x1)))
-# plot(y~x1, type="l", ylim=c(0,1))
-# points(hx~log(trun_x))
+a=opt1$par[1]
+b=opt1$par[2]
+x1=seq(0.0001, 75, 0.0001)
+y<-exp(a + b * log(x1)) / (1 + exp(a + b * log(x1)))
 
-# plot(hx~trun_x, log="x")
-# points(y~(x1), type="l", col="blue")
+plot(trun_x, hx, xlab="Num. parasites", ylab="Prob. Survival")
+lines(x1, y)
 
-# ?barplot()
-# barplot(trun_obs)
-
-# points(trun_pred)
-# dev.new()
 
 # # Loss of hosts due to parasites
 
-# k=fits$estimate[1]
-# mu=fits$estimate[2]
-# num_racoons = length(bp)
+k=fits$estimate[1]
+mu=fits$estimate[2]
+num_racoons = length(bp)
 
-# hx_func = function(x){
-# 	exp(a + b *log( x)) / (1 + exp(a + b *log( x)))
-# }
+norm_factor = 0.79899 # To account for the fact that we see mortality at one
+hx_func = function(x){
+	exp(a + b *log( x)) / (1 + exp(a + b *log( x))) / norm_factor
+}
 
-# Fx_func = function(x){
-# 	dnbinom(x, size=k, mu=mu)* num_racoons
-# }
+Fx_func = function(x){
+	dnbinom(x, size=k, mu=mu) * num_racoons
+}
 
 # # Calculating host mortality due to parasites
-# xs = seq(0, max(trun_x), 1)
-# Z0 = 1 - (sum(hx_func(xs) * Fx_func(xs)) / sum(Fx_func(xs)))
-# #something not quite right with this now that it is log
+xs = seq(1, max(trun_x), 1)
+numerator = c(Fx_func(0), Fx_func(xs)) * c(1, hx_func(xs))
+sum_numerator = sum(numerator)
 
-# xs = seq(1, max(trun_x), 1)
-# Z1 = 1 - (sum(hx_func(xs) * Fx_func(xs)) / sum(Fx_func(xs)))
-# Z1
+denom = c(Fx_func(seq(0, max(trun_x), 1)))
+sum_denom = sum(denom)
+
+Z0 = 1 - sum_numerator / sum_denom
+
+Z1 = 1 - (sum(hx_func(xs) * Fx_func(xs)) / sum(Fx_func(xs)))
+
+Z2 = 1 - (sum(xs * hx_func(xs) * Fx_func(xs)) / sum(xs * Fx_func(xs)))
 
 
-# These values don't make sense, try log 2 binning to account for large parasite numbers
-
-
-#hist(data$adBPtot, col="black", xlab="Adult Parasites", main="",
-#	cex.lab=1.5, cex.axis=2, freq=FALSE, breaks=20)
+# What are some of the problems with this method
+# 1.  If you have juvenile mortality for reasons other than parasite mortality
+# you will overestimate the the probability of death caused by parasites.
+# 2.  Can we come up with a way to control for other factors of mortality?
+# 3. What about the relationship betwen age structure and parasite load?
+# Or hetergeneity and parasite load?  There is a distribution of parasites
+# across hosts that differes by age class.  This is where you will begin to
+# loose the power to detect these changes if you only have one sample.
+#  4.
+# This is not incorporated.  There is a distribution of parasites across
